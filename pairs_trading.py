ae0742156cdf4c11488fc18dd763a6c402e0244e
suspendedbelief1iii <jayglee11@gmail.com>
2025-06-07 23:58:01 -0700

    add: min and max functions

--- pairs_trading.py
+++ pairs_trading.py
@@ -4,21 +4,19 @@ import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+
 # Create pandas DataFrame with various financial institutions using yfinance.
 # 
 # Downloading data for Visa, Mastercard, Capital One, Chase, Citigroup, Wells Fargo, 
 # American Express, Goldman Sachs, U.S. Bank, and Bank of America from June 1, 2024 to June 1, 2025.
-df = yf.download(['V', 'MA', 'COF', 'JPM', 'C', 'WFC', 'AXP', 'GS', 'USB', 'BAC'], start='2024-06-01', end='2025-06-01', auto_adjust = False)
+# 
+# Note: Only the daily adjusted close price column per each stock will be considered.
+stocks = ['AXP','BAC','C','COF','GS','JPM','MA','USB','V','WFC']
+df = yf.download(stocks, start='2024-06-01', end='2025-06-01', auto_adjust=False)['Adj Close']
 
-# Drop all columns except for the adj close price.
-df.drop(columns = ['Open', 'High', 'Low','Close', 'Volume'], axis = 1, inplace = True)
+# Export dataframe of all imported stocks.
+df.to_csv("df.csv")
 
-# Drop the top or title column of the dataframe.
-df = df.droplevel(0, axis = 1)
-
-# Detail the most important figure information per each stock, including mean, standard deviation, min, and max.
-info = df.describe()
-info.to_csv("info.csv")
 
 # Form a traditional correlation heatmap given current stocks as a proof of concept.
 # 
@@ -27,4 +25,58 @@ info.to_csv("info.csv")
 plt.figure(figsize=(10, 8))
 sb.heatmap(df.corr(), annot=True, cmap='Spectral')
 plt.title('Correlation Heatmap')
+
+# Save correlation heatmap to file directory.
 plt.savefig('heatmap.png', dpi=300, bbox_inches='tight')
+
+
+# Using greedy methods, find min, max, variance, mean, standard deviation, 
+# and interquartile range of stocks and download to csv for future use. 
+# 
+# Find minimum and maximum of each stock.
+#
+# Function to find minimum daily adjusted close price.
+def minimum(data):
+    min = data[0]
+    for n in data:
+        if n < min:
+            min = n
+    return min
+
+# Function to find maximum daily adjusted close price.
+def maximum(data):
+    max = data[0]
+    for n in data:
+        if n > max:
+            max = n
+    return max
+
+# Find variance of each stock.  *** TO BE COMPLETED
+
+# Find mean price of each stock.  *** TO BE COMPLETED
+
+# Find standard deviation of each stock.  *** TO BE COMPLETED
+
+# Find interquartile range of each stock.  *** TO BE COMPLETED
+
+# Combine statistics into csv.
+#
+# Create dataframe with columns as the tickers and the rows as the statistics.
+stats = pd.DataFrame(columns=stocks, index=['Min','Max'])
+
+# Iterate through stocks and compute each statistic.
+for ticker in stocks:
+    data = df[ticker].tolist()
+
+    stats[ticker] = [minimum(data), maximum(data)]
+
+# Save statistics to file directory. 
+print(stats)
+stats.to_csv("stats.csv")
+
+
+# Compute pearson correlation, spearman correlation, and kendall correlation.  *** TO BE COMPLETED
+
+# Compute engle-granger cointegration.  *** TO BE COMPLETED
+
+# Utilize pairs trading methods to find optimal pairs trading strategy.  *** TO BE COMPLETED
