f320d6f0bf7d76cd064689886e71ff407e92daf3
Reyli Hernandez <reryhern18@gmail.com>
2025-06-14 14:23:57 -0700

    removing a file that wasnt supposed to be there

--- Pairs-Trading.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-import numpy as np
-
-# Create pandas DataFrame with various financial institutions using yfinance.
-# Downloading data for Visa, Mastercard, Discover Financial Services, JPMorgan Chase, Citigroup, Wells Fargo, American Express, Goldman Sachs, U.S. Bancorp, and Bank of America from June 1, 2024 to June 1, 2025.
-df = yf.download(['V', 'MA', 'DFS', 'JPM', 'C', 'WFC', 'AXP', 'GS', 'USB', 'BAC'], start='2024-06-01', end='2025-06-01')
-
-print(df.head())
